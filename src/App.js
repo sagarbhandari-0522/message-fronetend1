a9fda5279cfc0e860822d5976dd811a19e33b40d
Sagar Bhandari <sagarvandari1@gmail.com>
2024-07-04 12:04:26 +1200

    add number

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Taskbar from "./components/Taskbar";
 import {Route, Routes} from "react-router";
 import Home from "./components/Home";
 import Chatroom from "./components/Chatroom";
+import NumberSumUp from "./components/NumberSumUp";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
             <Route  path="/" element={<Home/>}/>
             <Route path="/chatroom" element={<Chatroom/>}/>
+            <Route path="/NumberSum" element={<NumberSumUp/>}/>
         </Routes>
      <button className={"btn btn-primary"}>Click Me</button>
     </div>
---
+++ src/components/NumberSumUp.js
@@ -0,0 +1,57 @@
+import React, {useState} from 'react';
+import axios from "axios";
+
+function NumberSumUp(props) {
+    const[startNum, setStartNum] =useState(0)
+    const[endNum, setEndNum]=useState(0)
+    const[result, setResult] = useState(0)
+
+    function start_number_handler(e)
+    {
+        setStartNum(e.currentTarget.value)
+    }
+    function end_number_handler(e)
+    {
+         setEndNum(e.currentTarget.value)
+    }
+    function calc()
+    {
+            let data = {
+              "start": startNum,
+              "end": endNum
+            };
+            let config = {
+              method: 'post',
+              maxBodyLength: Infinity,
+              url: 'https://message-nu-eight.vercel.app//chat/sum_numbers/',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              data : data
+            };
+
+            axios.request(config)
+            .then((response) => {
+              console.log(JSON.stringify(response.data));
+              setResult(response.data.result)
+
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+
+    }
+
+    return (
+        <div>
+            <p>Start Number: <input type={"number"} id={"startNum"}/></p>
+            <p>End Number: <input type={"number"} id={"endNum"}/></p>
+            <p><button id={"calbtn"} onClick={calc}>Calculate</button></p>
+            <p>Result: {result}</p>
+
+
+        </div>
+    );
+}
+
+export default NumberSumUp;
