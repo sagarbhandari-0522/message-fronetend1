7a204428dacec1f4282661f5296295e6ebd21383
Sagar Bhandari <sagarvandari1@gmail.com>
2024-07-08 17:51:38 +1200

    login page for user

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from "react-router";
 import Home from "./components/Home";
 import Chatroom from "./components/Chatroom";
 import NumberSumUp from "./components/NumberSumUp";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route  path="/" element={<Home/>}/>
             <Route path="/chatroom" element={<Chatroom/>}/>
             <Route path="/NumberSum" element={<NumberSumUp/>}/>
+            <Route path="/login" element={<Login/>}/>
         </Routes>
      <button className={"btn btn-primary"}>Click Me</button>
     </div>
